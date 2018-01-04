535459470c9631ea2abca4409124006d40fe73de
lihangyu1994 <809611707@qq.com>
2018-01-04 17:52:20 +0800

    定时器配置文件注释

--- src/com/lhy/utils/MyJobFactory.java
+++ src/com/lhy/utils/MyJobFactory.java
@@ -4,7 +4,11 @@ import org.quartz.spi.TriggerFiredBundle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
-
+/**
+ * quartz定时器想要使用注入的bean,需要这个配置类
+ * @author Administrator
+ *
+ */
 public class MyJobFactory extends SpringBeanJobFactory {
 
 	@Autowired
